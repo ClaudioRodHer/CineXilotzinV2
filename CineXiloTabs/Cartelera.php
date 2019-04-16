3a74b8874ac195e1b8379ad5fde0a25672ade4bb
root <root@localhost.localdomain>
2019-04-15 21:31:04 -0500

    v1

---
+++ CineXiloTabs/Cartelera.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Cartelera</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Bootstrap CSS LOCAL -->
+    <link rel="stylesheet" href="../CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="../CSS/UsersTabs/HomeCSS.css">
+
+    <!--se cargan los iconos(todos) -->
+    <link rel="stylesheet" href="../CSS/css/all.css">
+    <!-- iconos en linea     -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+     
+</head>
+  <body>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Xilo</a>
+          <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+              aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavId">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li class="nav-item">
+                      <a class="nav-link" href="Home.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="Funciones.php">Funciones</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link active" href="Cartelera.php">Cartelera</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="Dulceria.php">Dulceria</a>
+                  </li>
+                  <!-- <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="Dulceria.php" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dulceria</a>
+                      <div class="dropdown-menu" aria-labelledby="dropdownId">
+                          <a class="dropdown-item" href="Dulceria.php">Dulces</a>
+                          <a class="dropdown-item" href="#">Snacks</a>
+                          <a class="dropdown-item" href="#">Comida</a>
+                          <a class="dropdown-item" href="#">Bebidas</a>
+                      </div>
+                  </li> -->
+                  <li class="nav-item">
+                      <a class="nav-link" href="Promociones.php">Promociones</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="Historia.php">Informacion</a>
+                  </li>
+                 
+              </ul>
+              
+              <ul class="nav navbar-nav navbar-right">
+              
+                        <li><a href="https://www.facebook.com/CineTeatroXilotzin/" target="_blank"
+                        data-toggle='popover' title='Facebook'><img src='../Resources/Icons/facebook.png'>
+                               </a></li>
+                        <li><a href="https://www.instagram.com/explore/locations/268154316896875/cine-teatro-xilotzin/" target="_blank"
+                        data-toggle='popover' title='Instagram'><img src='../Resources/Icons/instagram.png' >
+                              </a></li>
+                    </ul>
+                  <!-- <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+              
+          </div>
+      </nav>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- local -->
+    <script src="../JS/jquery.js"></script>
+    <script src="../JS/popper.min.js"></script>
+    <script src="../JS/bootstrap.min.js"></script>
+</body>
+</html>
