a309a0e91b8c63a75456b0b829b9a6e75e03f81d
edu <miguelmartinezeduardo5c@gmail.com>
2019-04-25 10:28:00 -0500

    laloparte

--- CineXiloTabs/Funciones.php
+++ CineXiloTabs/Funciones.php
@@ -1,20 +1,20 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Fuunciones</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!--Lalo-->
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- Bootstrap CSS LOCAL -->
-    <link rel="stylesheet" href="../CSS/bootstrap.min.css">
+        <title>Fuunciones</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="../CSS/UsersTabs/HomeCSS.css">
-</head>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Bootstrap CSS LOCAL -->
+        <link rel="stylesheet" href="../CSS/bootstrap.min.css">
+
+        <link rel="stylesheet" href="../CSS/UsersTabs/HomeCSS.css">
+   </head>
   <body>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
           <a class="navbar-brand" href="#">Xilo</a>
           <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
               aria-expanded="false" aria-label="Toggle navigation">
@@ -64,14 +64,53 @@
               
           </div>
       </nav>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <!-- local -->
-    <script src="../JS/jquery.js"></script>
-    <script src="../JS/popper.min.js"></script>
-    <script src="../JS/bootstrap.min.js"></script>
-</body>
+      <header>
+        <h1></h1>
+
+      </header>
+
+
+      <main>
+          <section id="contenido1">
+                <hr>
+                <hr>
+
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 text-center">
+                            <h1>HOY</h1>
+                            <img src="img/homero3.png" alt=":(">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, aperiam.</p>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 text-center">
+                            <h1>ESTA SEMANA</h1>
+                            <img src="img/homero3.png" alt=":(">
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, minima!</p>
+
+                        </div>
+
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 text-center">
+                            <h1>ESTE MES</h1>
+                            <img src="img/homero3.png" alt=":(">
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, dolore?</p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+      
+      
+        </main>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <!-- local -->
+        <script src="../JS/jquery.js"></script>
+        <script src="../JS/popper.min.js"></script>
+        <script src="../JS/bootstrap.min.js"></script>
+    </body>
 </html>
